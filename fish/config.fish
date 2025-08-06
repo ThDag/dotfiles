3932c0764747c101ecbdb3db779ec07c8fb55245
taha <tahadoktor@gmail.com>
2025-08-06 13:58:05 +0300

    changed language and many other stuff

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if type -q eza
+    alias ll "eza -l -b --git -h --no-permissions --icons"
+    alias lla "ll -a"
+end
+
+alias cd z
+
+set -x LANG en_US.UTF-8
+
 zoxide init fish | source
+fish_vi_key_bindings
