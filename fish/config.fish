802076f84ca1a6a4d678af53e67b60657b6d826b
taha <tahadoktor@gmail.com>
2026-01-12 22:24:40 +0300

    fish greeting added

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    fortune | cowsay
+end
+
 if type -q eza
     alias ll "eza -l -b --git -h --no-permissions --icons --group-directories-first"
     alias lla "ll -a"
@@ -23,5 +27,38 @@ set -x EDITOR /opt/homebrew/bin/nvim
 set -x VISUAL /opt/homebrew/bin/nvim
 set -x FZF_DEFAULT_OPTS "--layout=reverse"
 # fzf-fish layout
+
+function tarage
+    set item $argv[1]
+    if test -d $item
+        set tarfile "$item.tar.gz"
+        tar -czf $tarfile $item
+        set target $tarfile
+    else
+        set target $item
+    end
+
+    set archive "$target.age"
+    age -p -o $archive $target
+    rm $target
+    echo "Encrypted to $archive"
+end
+
+function ungage
+    set archive $argv[1]
+    # remove .age suffix for decrypted output
+    set outfile (string replace -r '\.age$' '' $archive)
+
+    # decrypt
+    age -d -o $outfile $archive
+
+    # if it's a tar.gz, extract it
+    if string match -r '\.tar\.gz$' $outfile
+        tar -xzf $outfile
+        rm $outfile # remove tar after extracting
+    end
+
+    echo "Decrypted (and extracted if tar) from $archive"
+end
 
 eval (direnv hook fish)
